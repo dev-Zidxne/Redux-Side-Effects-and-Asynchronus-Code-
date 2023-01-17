9b9f8c9a1cfef504ae9d465e8a4dead9296d9070
Zidane Innis <zidaneinnis@gmail.com>
2023-01-16 22:05:46 -0500

    Refresher / Practice: Part 1/2

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,22 @@
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import cartSlice from "../store/cart-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { Fragment } from "react";
+import { cartAction } from "../store/cart-slice";
+
+const Cart = (props) => {
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        <CartItem
+          item={{ title: "Test Item", quantity: 3, total: 18, price: 6 }}
+        />
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;

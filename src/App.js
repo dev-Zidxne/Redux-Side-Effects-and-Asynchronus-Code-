9741a08d50049fb1bc929589e064849063653019
Zidane Innis <zidaneinnis@gmail.com>
2023-01-17 10:54:07 -0500

    Using useeffect with redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -6,6 +6,25 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://food-order-app-b603f-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      const responseData = await response.json();
+    };
+  }, [cart]);
+
   return (
     <Fragment>
       <Layout>

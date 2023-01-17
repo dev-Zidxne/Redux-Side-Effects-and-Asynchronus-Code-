dfaee406cd34c48f9f2ecda9065d292827b146fa
Zidane Innis <zidaneinnis@gmail.com>
2023-01-17 11:11:09 -0500

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Fragment, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./components/store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.setNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
       const response = await fetch(
         "https://food-order-app-b603f-default-rtdb.firebaseio.com/cart.json",
         {
@@ -18,15 +31,45 @@ function App() {
         }
       );
       if (!response.ok) {
-        throw new Error("Sending cart data failed.");
+        throw Error("Sending cart data failed");
       }
 
       const responseData = await response.json();
+
+      dispatch(
+        uiActions.setNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully",
+        })
+      );
     };
-  }, [cart]);
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error",
+          message: "Sent cart data failed",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Layout>
         {showCart && <Cart />}
         <Products />
